1c9fc86859a70f7e5a368838671b0263272e4812
daniel040506 <danielk040506@gmail.com>
2024-08-28 01:00:11 -0700

    began work on creating express API endpoints

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express');
-const mysql = require('mysql2');
-const bodyParser = require('body-parser');
+
 
 const app = express();
 const port = 3000;
 
+
+
+const cors = require('cors');
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+
+
 // Middleware
 app.use(bodyParser.json());
+app.use(cors());
 
 // Create MySQL connection
 const db = mysql.createConnection({
@@ -29,3 +36,54 @@ db.connect((err) => {
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
+
+app.get('/api/products', (req, res) => {
+    const sql = 'SELECT * FROM products';
+    db.query(sql, (err, results) => {
+        if (err) throw err;
+        res.json(results);
+    });
+});
+
+app.get('/api/products/:id', (req, res) => {
+    const { id } = req.params;
+    const sql = 'SELECT * FROM products WHERE id = ?';
+    db.query(sql, [id], (err, result) => {
+        if (err) throw err;
+        res.json(result[0]);
+    });
+});
+
+app.post('/api/products', (req, res) => {
+    const { name, description, price, image_url } = req.body;
+    const sql = 'INSERT INTO products (name, description, price, image_url) VALUES (?, ?, ?, ?)';
+    db.query(sql, [name, description, price, image_url], (err, result) => {
+        if (err) throw err;
+        res.json({ id: result.insertId, ...req.body });
+    });
+});
+
+app.put('/api/products/:id', (req, res) => {
+    const { id } = req.params;
+    const { name, description, price, image_url } = req.body;
+    const sql = 'UPDATE products SET name = ?, description = ?, price = ?, image_url = ? WHERE id = ?';
+    db.query(sql, [name, description, price, image_url, id], (err, result) => {
+        if (err) throw err;
+        res.json({ message: 'Product updated', ...req.body });
+    });
+});
+
+app.delete('/api/products/:id', (req, res) => {
+    const { id } = req.params;
+    const sql = 'DELETE FROM products WHERE id = ?';
+    db.query(sql, [id], (err, result) => {
+        if (err) throw err;
+        res.json({ message: 'Product deleted' });
+    });
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
